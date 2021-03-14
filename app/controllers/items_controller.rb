f435d4696d36e2f7116c1354680cd6b56725dd0a
tomohito0358 <tomohito0358@gmail.com>
2021-03-14 20:52:38 +0900

    ルーティング、アクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+  end
+  
+
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :category_id, :status_id, :shipping_id, :prefecture_id, :date_of_shipment_id, :price).merge(user_id: current_user.id)
   end
