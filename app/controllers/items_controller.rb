fad0c5bc926f00b3c885444e43b7f6f995fe79c5
tomohito0358 <tomohito0358@gmail.com>
2021-03-14 13:02:07 +0900

    商品詳細表示機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, ]
+  before_action :authenticate_user!, only: [:new, :create ]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,12 +20,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-  end
-
-  def edit
-  end
-
-  def destroy
   end
 
   def item_params
