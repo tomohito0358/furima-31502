4a768a985629c2556c7467f41b818b5ce746bf7d
tomohito0358 <tomohito0358@gmail.com>
2021-03-26 21:19:35 +0900

    heroku対応のための変更点作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :first_name, :last_name, :first_name_read, :last_name_read, :birth_day ])
